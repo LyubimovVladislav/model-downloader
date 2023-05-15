2811d1fabc4b35027ee5a035e1e0652b3a9b66f4
LyubimovVladislav <grifon7676@gmail.com>
2023-05-15 16:51:15 +0300

    Fetch some model params

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from os.path import isdir, isfile
 from os import makedirs
 from colorama import Fore, Style, init as colorama_init
 from diffusers.pipelines.stable_diffusion.convert_from_ckpt import download_from_original_stable_diffusion_ckpt
+import torch
+import re as regex
 
 API_ENDPOINT = "https://civitai.com/api/v1/models/"
+image_size = 512
+fp_half_precision = False
 
 
 def download(url: str, filename: str):
@@ -40,21 +44,30 @@ def make_arg_parser():
         type=str,
         default=None,
         help='Path to the output model.')
+    parser.add_argument(
+        '--safety_checker', '-s',
+        action="store_true",
+        default=False,
+        help='Determines whether to load the safety checker. Defaults to False. '
+             'Apply the argument to load the safety checker.'
+    )
     return parser.parse_args()
 
 
-def file_not_exist_or_can_override():
-    if not isfile(checkpoint):
+def file_not_exist_or_can_override(filename: str):
+    if not isfile(filename):
         return True
-    print(f'{checkpoint} already exist. Override the file?')
+    print(f'{filename} already exist. Override the file?')
     while True:
-        user_decision = input('Type "y" to download and override the checkpoint '
-                              'or "n" to continue with saved checkpoint')
+        user_decision = input('Type y/yes to download and override the checkpoint, '
+                              'n/no to continue with saved checkpoint or a/abort to exit the program.')
         user_decision = user_decision.lower().strip()
-        if user_decision == 'y':
+        if user_decision == 'y' or user_decision == 'yes':
             return True
-        if user_decision == 'n':
+        if user_decision == 'n' or user_decision == 'no':
             return False
+        if user_decision == 'a' or user_decision == 'abort':
+            exit(0)
 
 
 def exit_with_error(err: str):
@@ -78,20 +91,42 @@ if __name__ == "__main__":
     request = requests.get(API_ENDPOINT + model_id)
     response = request.json()
 
+    if response['modelVersions'][0]['baseModel'].split()[-1] == '768':
+        image_size = 768
+    if response['modelVersions'][0]['files'][0]['metadata']['fp'] == 'fp16':
+        fp_half_precision = True
+    repo_name = regex.sub('[\\\/:*?"<>| ]*', '', response['name'])
+    version_name = response['modelVersions'][0]['name'].replace(' ', '')
+    checkpoint_name = response['modelVersions'][0]['files'][0]['name']
+
+    dir_name = f'{model_id}_{repo_name}_{version_name}'
+    checkpoint_name = f'{model_id}_{checkpoint_name}'
+    checkpoint_format = response['modelVersions'][0]['files'][0]['metadata']['format']
+
     if request.status_code != 200:
         exit_with_error('The provided url is not valid.')
     if response['type'] == 'LORA':
-        exit_with_error('Unsupported checkpoint file.')
+        exit_with_error('Unsupported checkpoint type.')
+    if checkpoint_format != 'SafeTensor':
+        exit_with_error('Unsupported checkpoint format.')
 
     download_url = response['modelVersions'][0]['files'][0]['downloadUrl']
-    checkpoint = f'checkpoints/{model_id}.safetensors'
+    checkpoint = f'checkpoints/{checkpoint_name}'
 
-    if file_not_exist_or_can_override():
+    if file_not_exist_or_can_override(checkpoint):
         download(download_url, checkpoint)
 
     if not args.output:
-        args.output = f'models/{model_id}/'
+        args.output = f'models/{dir_name}/'
 
     print('Note that the process may take up to hours')
-    pipe = download_from_original_stable_diffusion_ckpt(checkpoint_path=checkpoint, from_safetensors=True)
+    pipe = download_from_original_stable_diffusion_ckpt(
+        checkpoint_path=checkpoint,
+        from_safetensors=True,
+        load_safety_checker=args.safety_checker,
+        image_size=image_size)
+
+    if fp_half_precision:
+        pipe.to(torch_dtype=torch.float16)
+
     pipe.save_pretrained(save_directory=args.output)
